ffda7562dc9e6dac681bcdf9b75f4b13a3cee41c
adriandcoding <158279702+adriandcoding@users.noreply.github.com>
2024-04-28 22:55:16 +0200

    Add files via upload
    Ejercicio realizado con typescript y funciones

---
+++ typescript-sandbox/index.js
@@ -0,0 +1,13 @@
+/*Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños.
+
+Solo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero.
+Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €.
+Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa.
+Utiliza JavaScript para hacer el cálculo y mostrar el resultado por consola.*/
+function cuentaTotal(amigos, cuenta_a_pagar) {
+    var resultado = 0;
+    var bebidas = 18;
+    resultado = (cuenta_a_pagar - bebidas) / amigos;
+    return resultado;
+}
+console.log(cuentaTotal(6, 120));
